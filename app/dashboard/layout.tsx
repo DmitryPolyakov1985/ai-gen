889bab3b93aad90d4536fb3c22b733cdb9a6500c
Dmitry <dmitry@smilecdr.com>
2025-01-22 00:11:24 -0700

    displayed template menus

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,15 +11,6 @@ function DashboardLayout({
         <SideNav />
       </div>
       <div className="col-span-3">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptas
-          eligendi delectus provident corrupti, modi nesciunt incidunt
-          doloremque dolorum, totam accusantium cumque officia reprehenderit
-          harum temporibus. Optio facilis excepturi dolorem enim velit commodi
-          blanditiis quis tempora, incidunt nam. Deleniti, officia pariatur!
-          Fugiat, labore! Accusantium aperiam eius, aut perspiciatis harum
-          suscipit.
-        </p>
         <main>{children}</main>
       </div>
     </div>
